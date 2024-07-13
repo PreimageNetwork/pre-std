8149f9459c898239ddd7639c85f0edde72063d34
Brice <briceyan3.0@gmail.com>
2024-07-13 18:04:01 +0800

    Update interface and remove upgradeables for now

--- src/examples/Hello.sol
+++ src/examples/Hello.sol
@@ -14,14 +14,14 @@ contract Hello is IMintableSet, ISetProvider, Set {
     address _relRegistry;
     uint64 _kind;
 
-    constructor() Set("hello", msg.sender) {
+    constructor() Set(msg.sender) {
         _kind = 18;
     }
 
     function mint(uint64 id) external override returns (uint64) {
         if (id != 0) revert NoSpecifyingId();
         id = _nextId();
-        ObjectMeta memory meta = ObjectMeta({rev: 1, krev: 0, srev: 0, cap: 0, kind: _kind, set: _self});
+        ObjectMeta memory meta = ObjectMeta({rev: 1, krev: 0, srev: 0, cap: 0, kind: _kind, set: _set});
         bytes32[] memory state = new bytes32[](0);
         _create(id, meta, state, msg.sender);
         emit URI(_pimUri(id, meta.rev), uint256(id));
@@ -39,9 +39,10 @@ contract Hello is IMintableSet, ISetProvider, Set {
         address owner,
         uint64 universe,
         uint64 self,
-        string memory baseUri
+        string memory uriBase,
+        string memory uriQuery
     ) external override returns (bytes4) {
-        __Set_init(owner, universe, self, baseUri);
+        __Set_init(owner, universe, self, uriBase, uriQuery);
         _setRegistry = setRegistry;
         _kindRegistry = kindRegistry;
         _relRegistry = relRegistry;
